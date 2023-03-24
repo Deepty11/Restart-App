09f03d736c1b61b4b8c6a9d59c973e601cf5bf7c
Rehnuma <rehnuma.reza@stitelnetworks.com>
2023-03-24 21:17:35 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Rehnuma Reza on 24/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
