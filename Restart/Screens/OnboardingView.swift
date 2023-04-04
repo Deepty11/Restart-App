82ca0ffc0dc627abe9d482a2e64bc52fc4f76a0d
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2023-04-04 19:03:10 +0600

    Preparing Onboarding Screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -10,15 +10,54 @@ import SwiftUI
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     var body: some View {
-        VStack(spacing: 20){
-            Text("OnboardingView")
-            Button("Start") {
-            isOnboardingViewActive = false
-            }.padding(10)
-            .border(.black)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(edges: .all)
+            
+            
+            VStack(spacing: 60) {
+                Spacer()
+                //MARK: - HEADER
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                It's not how much we love
+                But how much love we put into giving
+                """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }
+                //MARK: - CENTER
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 20)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable() // if not resizable() modifier called, no change in image size will be refleacted
+                        .scaledToFit()
+                    
+                }
+                
+                Spacer()
+                //MARK: - FOOTER
+            }
+           
             
         }
     }
+
 }
 
 struct OnboardingView_Previews: PreviewProvider {
