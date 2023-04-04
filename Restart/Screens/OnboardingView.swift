0244fc30ffcd11ad51c301be6fcb3d3c2a854b98
Rehnuma <rehnuma.reza@stitelnetworks.com>
2023-04-04 22:14:31 +0600

    Onboarding Screen Prepared

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -52,6 +52,59 @@ struct OnboardingView: View {
                 
                 Spacer()
                 //MARK: - FOOTER
+                ZStack{
+                    //MARK: -Parts of the component
+                    // 1. STatic background
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. Call-to-action (static)
+                    
+                    Text("Get Started")
+                        .foregroundColor(.white)
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .offset(x:20)
+                    // 3. Capsule shape ( dynamic width)
+                    
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                        
+                    }
+                    
+                    // 4. Draggable circle shape
+                    
+                    HStack {
+                        ZStack{
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                            Capsule()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
             }
            
             
